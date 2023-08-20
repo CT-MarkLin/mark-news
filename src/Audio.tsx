e720a7a53c725fd9af718b7723c69dbb536e9069
CT-MarkLin <52391225+CT-MarkLin@users.noreply.github.com>
2023-08-21 00:12:02 +0800

    1 file modified, 3 added

--- src/Audio.tsx
+++ src/Audio.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
 interface IAudio {
   data: string;
@@ -6,15 +6,16 @@ interface IAudio {
 }
 
 export const Audio: FC<IAudio> = ({ data, onEnd }) => {
-  // const [snippets, setSnippets] = useState<string[]>([]);
-  // const [readIndex, setReadIndex] = useState<string[]>([]);
+  const [snippets, setSnippets] = useState<string[]>([]);
+  const [readIndex, setReadIndex] = useState<number>(0);
 
   const audioRef = useRef<HTMLMediaElement>(null);
   useEffect(() => {
     if (!data || !audioRef.current) {
       return;
     }
-    const url = `https://mark-tts.deno.dev/?text=${data}`;
+    console.log({ snippets });
+    const url = `https://mark-tts.deno.dev/?text=${snippets[readIndex]}`;
     const audioObj = audioRef.current;
     audioObj.pause();
     audioObj.src = url;
@@ -23,15 +24,42 @@ export const Audio: FC<IAudio> = ({ data, onEnd }) => {
       // duration 变量现在存放音频的播放时长（单位秒）
     });
     audioObj.addEventListener('ended', () => {
-      onEnd();
+      if (readIndex === snippets.length - 1) {
+        onEnd();
+        return;
+      }
+      setReadIndex(readIndex + 1);
     });
     audioObj.addEventListener('error', () => {
       onEnd();
     });
-  }, [data, audioRef]);
+  }, [audioRef, snippets, readIndex]);
 
   useEffect(() => {
-    if (data.length <= 360) {
+    const maxLen = 360;
+    const audioObj = audioRef.current;
+    audioObj?.pause();
+    if (data.length <= maxLen) {
+      setSnippets([data]);
+      setReadIndex(0);
+    } else {
+      const paragraphs = data.split('\n');
+      const senteance = paragraphs.flatMap((item) => item.split('。'));
+      let temp = '';
+      let result: string[] = [];
+      for (let i = 0; i < senteance.length; i++) {
+        if (
+          (temp + senteance[i]).length >= maxLen ||
+          i === senteance.length - 1
+        ) {
+          result.push(temp);
+          temp = senteance[i];
+          continue;
+        }
+        temp += `。${senteance[i]}`;
+      }
+      setSnippets(result);
+      setReadIndex(0);
     }
   }, [data]);
 
