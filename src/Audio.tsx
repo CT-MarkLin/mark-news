dd0e297c5dfd6015d7f3283f36757b5c58b97fe8
CT-MarkLin <52391225+CT-MarkLin@users.noreply.github.com>
2024-08-25 11:17:52 +0800

    1 file modified

--- src/Audio.tsx
+++ src/Audio.tsx
@@ -32,7 +32,7 @@ export const Audio: FC<IAudio> = ({ data, index, onEnd }) => {
       return;
     }
     console.log({ snippets, readIndex });
-    const url = `https://mark-tts.deno.dev/?text=${snippets[readIndex]}`;
+    const url = `https://edge-tts.deno.dev/?text=${snippets[readIndex]}`;
     const audioObj = audioRef.current;
     // audioObj.pause();
     audioObj.src = url;
